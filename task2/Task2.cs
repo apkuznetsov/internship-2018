c782a751c51b78bd11060a0f3e31f667aaca0faa
Andrey <kouznts@ya.ru>
2021-03-18 10:43:54 +0400

    task 2 project template

---
+++ task2/Task2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace task2
+{
+    class Task2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
